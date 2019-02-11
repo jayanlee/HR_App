6301425a85b9a0a79ee06a83c58623e3986e9ac8
jayanlee <jayanlee93@gmail.com>
2019-02-11 12:13:05 +0530

    routing error

--- ng-pi-admin-master/src/app/app.routing.ts
+++ ng-pi-admin-master/src/app/app.routing.ts
@@ -3,22 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
     {
         path: '',
-        redirectTo: 'pages/index',
+        redirectTo: 'pages',
         pathMatch: 'full'
-      },
+    },
     {
         path: 'pages',
         children: [
             { path: '', redirectTo: 'index', pathMatch: 'full' },
-            { path: 'index', loadChildren: './index/index.module#IndexModule' },
-            { path: 'editor', loadChildren: './editor/editor.module#EditorModule' },
-            { path: 'icon', loadChildren: './icon/icon.module#IconModule' },
-            { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' },
-            { path: 'form', loadChildren: './form/form.module#FormModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'ui', loadChildren: './ui/ui.module#UIModule' },
-            { path: 'table', loadChildren: './table/table.module#TableModule' },
-            { path: 'menu-levels', loadChildren: './menu-levels/menu-levels.module#MenuLevelsModule' },
+            { path: 'index', loadChildren: './pages/index/index.module#IndexModule' },
+            { path: 'editor', loadChildren: './pages/editor/editor.module#EditorModule' },
+            { path: 'icon', loadChildren: './pages/icon/icon.module#IconModule' },
+            { path: 'profile', loadChildren: './pages/profile/profile.module#ProfileModule' },
+            { path: 'form', loadChildren: './pages/form/form.module#FormModule' },
+            { path: 'charts', loadChildren: './pages/charts/charts.module#ChartsModule' },
+            { path: 'ui', loadChildren: './pages/ui/ui.module#UIModule' },
+            { path: 'table', loadChildren: './pages/table/table.module#TableModule' },
+            { path: 'menu-levels', loadChildren: './pages/menu-levels/menu-levels.module#MenuLevelsModule' },
         ]
     }
 ];
